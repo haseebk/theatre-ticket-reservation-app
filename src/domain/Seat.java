7291aeae7ce69431ed60224251e00d8527938563
Haseeb <haseebadilkhan@gmail.com>
2020-11-25 17:45:00 -0700

    packaging classes

--- src/domain/Seat.java
+++ src/domain/Seat.java
@@ -1,14 +1,39 @@
 package domain;
 
 public class Seat {
+	private int row;
+	private int seatNum;
+	private boolean seatTaken;
+	
+	public Seat() {}
+	
+	// This method shows the seat selection?
+	public void showSeatSelection() {
+		// To be implemented
+	}
 
-    private bool seatTaken;
+	// Getters and Setters
+	public int getRow() {
+		return row;
+	}
 
-    public Seat(){
-        seatTaken = false;
-    }
+	public void setRow(int row) {
+		this.row = row;
+	}
 
-    public bool getSeatTaken(){return seatTaken;}
-    public void bookSeat(){seatTaken = true;}
-    public void vacateSeat(){seatTaken = false;}
+	public int getSeatNum() {
+		return seatNum;
+	}
+
+	public void setSeatNum(int seatNum) {
+		this.seatNum = seatNum;
+	}
+
+	public boolean isSeatTaken() {
+		return seatTaken;
+	}
+
+	public void setSeatTaken(boolean seatTaken) {
+		this.seatTaken = seatTaken;
+	}
 }
