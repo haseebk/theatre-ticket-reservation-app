53e0c444f206abb0694f29ab3ad09f30f1aff8fa
Haseeb <haseebadilkhan@gmail.com>
2020-11-26 20:02:53 -0700

    staging changes

---
+++ src/domain/Payment.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class Payment {
+	private int paymentID;
+	private int amount;
+	Card card;
+	
+}
--- src/domain/Ticket.java
+++ src/domain/Ticket.java
@@ -1,5 +1,59 @@
 package domain;
 
 public class Ticket {
+	private int ticketID;
+	private Payment payment;
+	private Movie the_movie;
+	private Showtime showtime;
+	private Seat seat;
+	
+	public Ticket() {}
+	
+	// This method sends the voucher code to the 
+	// email id in the argument
+	public void emailTicket(String email) {
+		// Needs to be implemented still
+	}
+	
+	// Getters and Setters
+	public int getTicketID() {
+		return ticketID;
+	}
+
+	public void setTicketID(int ticketID) {
+		this.ticketID = ticketID;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Movie getThe_movie() {
+		return the_movie;
+	}
+
+	public void setThe_movie(Movie the_movie) {
+		this.the_movie = the_movie;
+	}
+
+	public Showtime getShowtime() {
+		return showtime;
+	}
+
+	public void setShowtime(Showtime showtime) {
+		this.showtime = showtime;
+	}
+
+	public Seat getSeat() {
+		return seat;
+	}
+
+	public void setSeat(Seat seat) {
+		this.seat = seat;
+	}		
 
 }
