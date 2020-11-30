9d9ba0b8178988011b7445314a00cc043d694c92
vaibhavkpr <60018343+vaibhavkpr@users.noreply.github.com>
2020-11-30 15:50:29 -0700

    Edits to registerPage

---
+++ src/presentation/view/HomePage.java
@@ -0,0 +1,129 @@
+package presentation.view;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.*;
+import javax.swing.border.MatteBorder;
+
+import domain.model.BackEnd;
+
+public class HomePage extends JPanel {
+	/**
+	 * Back button
+	 */
+	private JLabel logoutButton;
+	/**
+	 * Cart button
+	 */
+	private JLabel cartButton;
+	/**
+	 * Announcement button
+	 */
+	private JLabel announcementButton;
+
+	public HomePage(JFrame frame, BackEnd backend) {
+		setLayout(null);
+		
+		// CREATE NO DATE SELECTED TEXT LABEL
+		JLabel noDateSelectedLabel = new JLabel("<html>"
+				+ "Please select a date."
+				+ "</html>");
+		noDateSelectedLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		noDateSelectedLabel.setForeground(Color.RED);
+		noDateSelectedLabel.setFont(new Font("Arial", Font.PLAIN, 13));
+		noDateSelectedLabel.setBounds(564, 235, 254, 45);
+//		noDateSelectedLabel.setVisible(false);
+		add(noDateSelectedLabel);
+		
+		// CREATE NO MOVIE SELECTED TEXT LABEL
+		JLabel noMovieSelectedLabel = new JLabel("<html>"
+				+ "Please select a movie."
+				+ "</html>");
+		noMovieSelectedLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		noMovieSelectedLabel.setForeground(Color.RED);
+		noMovieSelectedLabel.setFont(new Font("Arial", Font.PLAIN, 13));
+		noMovieSelectedLabel.setBounds(564, 235, 254, 45);
+//		noDateSelectedLabel.setVisible(false);
+		add(noMovieSelectedLabel);
+
+		// CREATE WELCOME TEXT LABEL
+		JLabel welcomeLabel;
+		if(backend.getCurrentUser() == null){
+			welcomeLabel = new JLabel("Welcome Guest");
+		}else {
+			welcomeLabel = new JLabel("Welcome " + backend.getCurrentUser().getF_name() + " " + backend.getCurrentUser().getL_name());
+		}
+		welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		welcomeLabel.setForeground(Color.WHITE);
+		welcomeLabel.setFont(new Font("Arial", Font.PLAIN, 25));
+		welcomeLabel.setBounds(20, 20, 500, 20);
+		add(welcomeLabel);
+
+		// CREATE LOGOUT BUTTON
+		logoutButton = new JLabel("");
+		logoutButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		logoutButton.setToolTipText("Logout");
+		logoutButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				backend.userLogout();
+				LoginPage loginPanel = new LoginPage(frame, backend);
+				frame.setContentPane(loginPanel);
+				frame.revalidate();
+			}
+		});
+		logoutButton.setBounds(30, 5, 50, 50);
+		logoutButton.setIcon(new ImageIcon(HomePage.class.getResource("/backButton.png")));
+		add(logoutButton);
+
+		// CREATE CART BUTTON
+		cartButton = new JLabel("");
+		cartButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		cartButton.setToolTipText("View Cart");
+		cartButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+			}
+		});
+		cartButton.setBounds(1366 - 32 - 30, 14, 32, 32);
+		cartButton.setIcon(new ImageIcon(HomePage.class.getResource("/cartButton.png")));
+		add(cartButton);
+
+		// CREATE ACCOUNCEMENT BUTTON
+		announcementButton = new JLabel("");
+		announcementButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		announcementButton.setToolTipText("View Announcements");
+		announcementButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+			}
+		});
+		announcementButton.setBounds(1366 - 32 - 90, 14, 32, 32);
+		announcementButton.setIcon(new ImageIcon(HomePage.class.getResource("/megaphoneButton.png")));
+		add(announcementButton);
+
+		// CREATE MOVIE CARD VIEW
+		JLabel movieCard = new JLabel("");
+		movieCard.setBounds(40, 132, 401, 564);
+		movieCard.setIcon(new ImageIcon(HomePage.class.getResource("/movieCard.png")));
+		add(movieCard);
+
+		// CREATE AUDITORIUM CARD VIEW
+		JLabel auditoriumCard = new JLabel("");
+		auditoriumCard.setBounds(525, 132, 801, 564);
+		auditoriumCard.setIcon(new ImageIcon(HomePage.class.getResource("/auditoriumCard.png")));
+		add(auditoriumCard);
+
+		// CREATE BACKGROUND VIEW
+		JLabel homeBackground = new JLabel("");
+		homeBackground.setBounds(-2, -1, 1366, 768);
+		homeBackground.setIcon(new ImageIcon(HomePage.class.getResource("/backgroundD.png")));
+		add(homeBackground);
+	}
+}
---
+++ src/presentation/view/LoginPage.java
@@ -0,0 +1,179 @@
+package presentation.view;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JPasswordField;
+import javax.swing.border.MatteBorder;
+
+import domain.model.BackEnd;
+import domain.model.User;
+
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Cursor;
+import javax.swing.SwingConstants;
+
+/**
+ * This class runs the Login panel view. It creates user name and password
+ * fields for user input, which is then verified with database to confirm login.
+ * 
+ * @author Haseeb Khan
+ *
+ */
+public class LoginPage extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * User name text field for user login input
+	 */
+	private JTextField usernameTextField;
+	/**
+	 * Password text field for user login input
+	 */
+	private JPasswordField passwordField;
+	/**
+	 * Display text label "Password"
+	 */
+	private JLabel passwordLabel;
+	/**
+	 * Display text label "Username"
+	 */
+	private JLabel usernameLabel;
+	JLabel submitLoginButton;
+	private JLabel registerButton;
+	private JLabel guestButton;
+
+	/**
+	 * Create the panel.
+	 * 
+	 * @param frame   frame that the panel is being placed onto
+	 * @param backend backend to obtain information and apply logic
+	 * @param auth    authorizer
+	 */
+//		public Login(JFrame frame, BackEnd backend, Authenticator auth) (to be added)
+
+	public LoginPage(JFrame frame, BackEnd backend) {
+		setLayout(null);
+
+		// CREATE GUEST LOGIN BUTTON
+		guestButton = new JLabel("CONTINUE AS GUEST");
+		guestButton.setHorizontalAlignment(SwingConstants.CENTER);
+		guestButton.setForeground(Color.GRAY);
+		guestButton.setFont(new Font("Arial", Font.BOLD, 11));
+		guestButton.setBounds(632, 502, 118, 14);
+		guestButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		guestButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				backend.guestUser();
+				HomePage homePanel = new HomePage(frame, backend);
+				frame.setContentPane(homePanel);
+				frame.revalidate();
+			}
+		});
+		add(guestButton);
+
+		// CREATE REGISTER BUTTON
+		registerButton = new JLabel("CREATE ACCOUNT");
+		registerButton.setHorizontalAlignment(SwingConstants.CENTER);
+		registerButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+//					Register registerPanel = new Register(frame, backend, auth);
+				RegisterPage registerPanel = new RegisterPage(frame, backend);
+				frame.setContentPane(registerPanel);
+				frame.revalidate();
+			}
+		});
+		registerButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		registerButton.setForeground(Color.GRAY);
+		registerButton.setFont(new Font("Arial", Font.BOLD, 11));
+		registerButton.setBounds(638, 524, 106, 14);
+		add(registerButton);
+
+		// CREATE USERNAME TEXT FIELD
+		usernameTextField = new JTextField();
+		usernameTextField.setToolTipText("Enter your username.");
+		usernameTextField.setFont(new Font("Arial", Font.PLAIN, 13));
+		usernameTextField.setBorder(new MatteBorder(0, 0, 3, 0, (Color) Color.LIGHT_GRAY));
+		usernameTextField.setForeground(Color.DARK_GRAY);
+		usernameTextField.setBackground(Color.WHITE);
+		usernameTextField.setBounds(564, 314, 254, 28);
+		usernameTextField.setOpaque(true);
+		usernameTextField.setColumns(10);
+		usernameTextField.setOpaque(true);
+		add(usernameTextField);
+
+		// CREATE PASSWORD TEXT FIELD
+		passwordField = new JPasswordField();
+		passwordField.setToolTipText("Enter your password.");
+		passwordField.setBorder(new MatteBorder(0, 0, 3, 0, (Color) Color.LIGHT_GRAY));
+		passwordField.setForeground(Color.BLACK);
+		passwordField.setBounds(564, 387, 254, 28);
+		add(passwordField);
+
+		// CREATE USERNAME TEXT LABEL
+		usernameLabel = new JLabel("Username");
+		usernameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		usernameLabel.setForeground(Color.WHITE);
+		usernameLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+		usernameLabel.setBounds(653, 299, 77, 14);
+		add(usernameLabel);
+
+		// CREATE PASSWORD TEXT LABEL
+		passwordLabel = new JLabel("Password");
+		passwordLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		passwordLabel.setForeground(Color.WHITE);
+		passwordLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+		passwordLabel.setBounds(653, 372, 77, 14);
+		add(passwordLabel);
+
+		// CREATE ERROR TEXT LABEL
+		JLabel invalidLoginErrorLabel = new JLabel("<html>"
+				+ "Entered login credentials do not match \r\na registered account in our system. (not yet implemented)"
+				+ "</html>");
+		invalidLoginErrorLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		invalidLoginErrorLabel.setForeground(Color.RED);
+		invalidLoginErrorLabel.setFont(new Font("Arial", Font.PLAIN, 13));
+		invalidLoginErrorLabel.setBounds(564, 235, 254, 45);
+		invalidLoginErrorLabel.setVisible(false);
+		add(invalidLoginErrorLabel);
+
+		// CREATE SUBMIT LOGIN BUTTON
+		submitLoginButton = new JLabel("");
+		submitLoginButton.setToolTipText("Login");
+		submitLoginButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		submitLoginButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String user = usernameTextField.getText();
+				String pass = String.valueOf(passwordField.getPassword());
+				System.out.println("User inputed:" + user + " " + pass);
+				if(backend.verifyLogin(user,pass) != null) {
+					HomePage homePanel = new HomePage(frame, backend);
+					frame.setContentPane(homePanel);
+				}else{
+					invalidLoginErrorLabel.setVisible(true);
+				}
+				frame.revalidate();
+			}
+		});
+		submitLoginButton.setBounds(564, 442, 254, 50);
+		submitLoginButton.setIcon(new ImageIcon(LoginPage.class.getResource("/enterButton.png")));
+		add(submitLoginButton);
+
+		// CREATE BACKGROUND VIEW
+		JLabel loginBackground = new JLabel("");
+		loginBackground.setBounds(-2, -1, 1366, 768);
+		loginBackground.setIcon(new ImageIcon(LoginPage.class.getResource("/backgroundA.png")));
+		add(loginBackground);
+	}
+}
