22567a39dd38f144c5622611ca240728e643aa1f
vaibhavkpr <60018343+vaibhavkpr@users.noreply.github.com>
2020-11-30 20:14:08 -0700

    Add files via upload

--- src/domain/model/Cart.java
+++ src/domain/model/Cart.java
@@ -3,36 +3,53 @@ package domain.model;
 import java.util.*;
 
 public class Cart {
-	private ArrayList<Booking> items_in_cart;
-
-	public Cart() {
-		items_in_cart = new ArrayList<Booking>();
+	private ArrayList<Ticket> items_in_cart;
+	private Payment payment;
+	
+	public Cart () {
+		items_in_cart =  new ArrayList<Ticket>(); 
 	}
-
+	
+	public Cart (ArrayList<Ticket> items, Payment pay) {
+		items_in_cart =  new ArrayList<Ticket>(); 
+		for(int i=0; i<items.size(); i++) {
+			items_in_cart.add(items.get(i));
+		}
+		setPayment(pay);
+	}
+	
 	// This method adds a ticket from the argument to the cart items list
-	public void addToCart(Booking item) {
+	public void addToCart(Ticket item) {
 		items_in_cart.add(item);
 	}
-
+	
 	// This method removes a ticket specified item from the cart
-	public void removeFromCart(Booking item) {
-		Iterator<Booking> it = items_in_cart.iterator();
-		while (it.hasNext()) {
-			Booking currentTicket = (Booking) it.next();
-			if (item.getBookingID() == currentTicket.getBookingID()) {
-				it.remove();
-				return;
-			}
-		}
-		return;
+	public void removeFromCart(Ticket item) {
+		Iterator<Ticket> it = items_in_cart.iterator(); 
+        while (it.hasNext()) {
+        	Ticket currentTicket = (Ticket)it.next();
+            if(item.getTicketID() == currentTicket.getTicketID()) {
+            	it.remove();
+            	return;
+            }
+        }
+        return;
 	}
 
 	// Getters and Setters
-	public ArrayList<Booking> getItems_in_cart() {
+	public ArrayList<Ticket> getItems_in_cart() {
 		return items_in_cart;
 	}
 
-	public void setItems_in_cart(ArrayList<Booking> items_in_cart) {
+	public void setItems_in_cart(ArrayList<Ticket> items_in_cart) {
 		this.items_in_cart = items_in_cart;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 }
--- src/domain/model/User.java
+++ src/domain/model/User.java
@@ -5,21 +5,20 @@ public class User {
 	 * Type of User
 	 */
 	String userType;
-
+	
 	/**
 	 * Constructor to set the type of user
-	 * 
 	 * @param t the user type
 	 */
-	User(String t) {
+	User(String t){
 		this.userType = t;
 	}
-
+	
 	// Getters and Setters
 	public String getUserType() {
 		return userType;
 	}
-
+	
 	public void setUserType(String userType) {
 		this.userType = userType;
 	}
